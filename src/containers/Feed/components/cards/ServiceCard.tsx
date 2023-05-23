66d88b7722e075ef01706bd65bd6f406fd8a1e32
varun7244 <72227999+varun7244@users.noreply.github.com>
2023-05-23 20:32:34 +0530

    Update ServiceCard.tsx

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -6,9 +6,6 @@ const ServiceCard: React.FC = () => {
   if (!CONFIG.projects) return null
   return (
     <>
-      <div className="p-1 mb-3 dark:text-white"></div>
-      <ul className="rounded-2xl mb-9 bg-white dark:bg-zinc-700 p-1 flex flex-col">
-      </ul>
     </>
   )
 }
