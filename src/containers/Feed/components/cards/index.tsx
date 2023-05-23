a9a141e9430e498528279a5ccb48fd20d8737943
varun7244 <72227999+varun7244@users.noreply.github.com>
2023-05-23 20:13:36 +0530

    Update index.tsx

--- src/containers/Feed/components/cards/index.tsx
+++ src/containers/Feed/components/cards/index.tsx
@@ -1,6 +1,5 @@
 import ProfileCard from './ProfileCard'
 import ContactCard from './ContactCard'
-import ServiceCard from './ServiceCard'
 import MobileProfileCard from './MobileProfileCard'
 
-export { ProfileCard, ContactCard, ServiceCard, MobileProfileCard }
+export { ProfileCard, ContactCard, MobileProfileCard }
