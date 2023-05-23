26e6543f0b3b92e46f3fe58301d4474023afbc90
varun7244 <72227999+varun7244@users.noreply.github.com>
2023-05-23 20:29:21 +0530

    Update ServiceCard.tsx

--- src/containers/Feed/components/cards/ServiceCard.tsx
+++ src/containers/Feed/components/cards/ServiceCard.tsx
@@ -3,10 +3,10 @@ import React from "react"
 import { AiFillCodeSandboxCircle } from "react-icons/ai"
 
 const ServiceCard: React.FC = () => {
-  if (CONFIG.projects) return null
+  if (!CONFIG.projects) return null
   return (
     <>
-      <div className="p-1 mb-3 dark:text-white">🌟 Servicer</div>
+      <div className="p-1 mb-3 dark:text-white"></div>
       <ul className="rounded-2xl mb-9 bg-white dark:bg-zinc-700 p-1 flex flex-col">
         <a
           href={`${CONFIG.projects[0].href}`}
